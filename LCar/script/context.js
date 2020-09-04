d219e2a0284e6be43deae8910dd64f623f32bc51
thuya66 <54479396+Thuya66@users.noreply.github.com>
2020-09-04 23:54:07 +0630

    lamborghi version-1.0

---
+++ LCar/script/context.js
@@ -0,0 +1,66 @@
+function showAventador()
+{
+	document.getElementById("adv_text").innerHTML = "CREATE YOUR<br>AVENTADOR";
+	document.getElementById("adv_img").src="res/lambo_context.png";
+}
+
+function showHuracan()
+{
+	document.getElementById("adv_text").innerHTML = "CREATE YOUR<br>HURACAN";
+	document.getElementById("adv_img").src="res/lamborghini-huracan.png";
+}
+
+function showUrus()
+{
+	document.getElementById("adv_text").innerHTML = "CREATE YOUR<br>URUS";
+	document.getElementById("adv_img").src="res/lamborghini-urus.jpg";
+}
+
+function goSuperTrofeo() {
+	window.location = 'https://squadracorse.lamborghini.com/';
+}
+
+function goGT3(){
+	window.location = 'https://squadracorse.lamborghini.com/';
+}
+
+function Giallo_Inti()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-giallo_tenerife-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function black()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-nero_granatus-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Bianco_Icarus()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-bianco_icarus-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Blu_Aegeus()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-blu_aegeus-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Viola_Pasifae()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-viola_pasifae-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Blu_Glauco()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-blu_aegeus-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Verde_Selvans()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-verde_selvans-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
+function Blu_Sideris()
+{
+	document.getElementById("car_png").src = "res/Huracan_Evo_Spyder_cc-blu_sideris-Giano-20-Silver-black_caliper-sceneplate_env.png";
+}
+
